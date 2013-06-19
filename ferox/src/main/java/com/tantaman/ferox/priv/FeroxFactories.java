ae340628422c422804a2cb9d03100ebaa3d4c9ec
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-06-18 22:23:29 -0400

    Removing bnd moving to p2

--- ferox/src/main/java/com/tantaman/ferox/priv/FeroxFactories.java
+++ ferox/src/main/java/com/tantaman/ferox/priv/FeroxFactories.java
@@ -1,7 +1,5 @@
 package com.tantaman.ferox.priv;
 
-import aQute.bnd.annotation.component.Component;
-
 import com.tantaman.ferox.FeroxChannelHandlerFactory;
 import com.tantaman.ferox.RouterBuilder;
 import com.tantaman.ferox.api.IChannelHandlerFactory;
@@ -9,7 +7,6 @@ import com.tantaman.ferox.api.IFeroxFactories;
 import com.tantaman.ferox.api.IRouter;
 import com.tantaman.ferox.api.IRouterBuilder;
 
-@Component
 public class FeroxFactories implements IFeroxFactories {
 
 	@Override
