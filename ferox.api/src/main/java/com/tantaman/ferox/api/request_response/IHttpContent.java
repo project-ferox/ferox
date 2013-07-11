f29c9d2a1da4a45bbe94036b7b605bf6642378e0
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-07-11 10:04:45 -0400

    Provided a way to get the raw content of the request body

--- ferox.api/src/main/java/com/tantaman/ferox/api/request_response/IHttpContent.java
+++ ferox.api/src/main/java/com/tantaman/ferox/api/request_response/IHttpContent.java
@@ -1,6 +1,8 @@
 package com.tantaman.ferox.api.request_response;
 
+import io.netty.buffer.ByteBuf;
+
 
 public interface IHttpContent extends IHttpReception {
-	
+	public ByteBuf getContent();
 }
--- ferox/src/main/java/com/tantaman/ferox/priv/HttpContent.java
+++ ferox/src/main/java/com/tantaman/ferox/priv/HttpContent.java
@@ -1,5 +1,7 @@
 package com.tantaman.ferox.priv;
 
+import io.netty.buffer.ByteBuf;
+
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +9,7 @@ import com.tantaman.ferox.api.request_response.IHttpContent;
 import com.tantaman.ferox.middleware.message_types.TrackedHttpRequest;
 
 public class HttpContent extends HttpReception implements IHttpContent {
+	private final io.netty.handler.codec.http.HttpContent content;
 	public HttpContent(io.netty.handler.codec.http.HttpContent content,
 			Map<String, String> urlParams,
 			Map<String, List<String>> query,
@@ -14,5 +17,11 @@ public class HttpContent extends HttpReception implements IHttpContent {
 			String catchall,
 			TrackedHttpRequest trackedRequest) {
 		super(content, urlParams, query, splats, catchall, trackedRequest);
+		this.content = content;
+	}
+	
+	@Override
+	public ByteBuf getContent() {
+		return content.content();
 	}
 }
